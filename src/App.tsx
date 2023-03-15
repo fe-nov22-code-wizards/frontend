cb0403bc9fc5f1da30782fc6c242ce1455f88642
Andrii Andriushchenko <andrewandrew1604@gmail.com>
2023-03-15 17:23:11 +0200

    add grid and delete redundant files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/Grid/Grid.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App Grid">
+      <h1
+        className="
+          App
+          Grid__item
+          Grid__item-1-4
+          Grid__item--tablet-1-12
+          Grid__item--desktop-1-24"
+      >
+        Hello, world!
+      </h1>
     </div>
   );
-}
+};
 
 export default App;
