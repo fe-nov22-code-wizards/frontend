618a4610785c74d9e9df2c8e08f2b63e0fcf9578
Andrii <kurash.andrii@gmail.com>
2023-03-16 12:55:00 +0200

    fixes linter mistake

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './components/Grid/grid.scss';
 import { PageNotFound } from './components/PageNotFound';
 
-
 export const App: React.FC = () => {
   return (
     <>
