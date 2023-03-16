286b17a91ea2490e29277124fcd66f115a50d9a7
Igor Kashchenko <blackangryrabbit@gmail.com>
2023-03-16 15:56:42 +0200

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
-import './components/Grid/grid.scss';
+import './components/Grid/Grid.scss';
 import { PageNotFound } from './components/PageNotFound';
 
 export const App: React.FC = () => {
   return (
     <>
-       <Header />
-       <h1>Hello, world!</h1>
-       <PageNotFound />
+      <Header />
+      <h1>Hello, world!</h1>
+      <PageNotFound />
     </>
   );
 };
