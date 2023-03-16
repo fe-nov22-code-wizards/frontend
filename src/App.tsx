f17ffb8ed08df763324ccb9ccd8ec6f5ba9d3867
Andrii <kurash.andrii@gmail.com>
2023-03-16 11:24:11 +0200

    Realised page-not-found

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import './components/grid/grid.scss';
+import './components/Grid/grid.scss';
+import { PageNotFound } from './components/PageNotFound';
 
 const App: React.FC = () => {
   return (
-    <div className="App grid">
-      <h1
-        className="
-          App
-          grid__item
-          grid__item-1-4
-          grid__item--tablet-1-12
-          grid__item--desktop-1-24"
-      >
-        Hello, world!
-      </h1>
-    </div>
+    <>
+      <PageNotFound />
+    </>
   );
 };
 
