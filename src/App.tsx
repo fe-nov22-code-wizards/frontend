d46b7fd19547a21caf3fa73b774233b7edf56cc0
Andrii Andriushchenko <andrewandrew1604@gmail.com>
2023-03-16 19:49:00 +0200

    Footer added and some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import './components/Grid/grid.scss';
+import './components/Grid/Grid.scss';
 import { PageNotFound } from './components/PageNotFound';
 import { ProductCardLayout } from './components/ProductCardLayout';
+import { Footer } from './components/Footer';
+import './App';
 
 export const App: React.FC = () => {
   return (
     <>
       <ProductCardLayout />
       <PageNotFound />
+      <Footer />
     </>
   );
 };
