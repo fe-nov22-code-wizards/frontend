418e0ac3055beff7dc8a32d956d1a996a7d4ac52
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-03-17 17:28:34 +0100

    fixes in fetch

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -4,12 +4,7 @@ const BASE_URL = 'https://api-gwis.onrender.com';
 
 // eslint-disable-next-line space-before-function-paren
 export const getAllPhones = async (): Promise<Phone[]> => {
-  const response = await fetch(`${BASE_URL}/phones`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const response = await fetch(`${BASE_URL}/phones`);
 
   return response.json();
 };
