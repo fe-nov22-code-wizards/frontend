cd752dd95ee192f2ace8ae080c1afb068ee5af26
Andrii <kurash.andrii@gmail.com>
2023-03-17 15:23:41 +0200

    Realised BannerSlider with TS and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BannerSlider } from './components/BannerSlider';
 import './components/Grid/grid.scss';
 import { PageNotFound } from './components/PageNotFound';
 import { ProductCardLayout } from './components/ProductCardLayout';
@@ -6,6 +7,7 @@ import { ProductCardLayout } from './components/ProductCardLayout';
 export const App: React.FC = () => {
   return (
     <>
+      <BannerSlider />
       <ProductCardLayout />
       <PageNotFound />
     </>
