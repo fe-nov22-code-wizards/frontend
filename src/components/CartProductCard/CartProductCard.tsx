5cf9c054e8d03961fa1464eadfb520485f24bb2b
Anna Inozemets <inoznetta@gmail.com>
2023-03-18 19:55:28 +0200

    Cart page became adaptive

--- src/components/CartProductCard/CartProductCard.tsx
+++ src/components/CartProductCard/CartProductCard.tsx
@@ -7,46 +7,51 @@ import './CartProductCard.scss';
 export const CartProductCard: React.FC = () => {
   return (
     <div className="cart_card grid__item--desktop-1-16">
-      <button className="cart_card-btn-delete">
-        <svg
-          width="10"
-          height="10"
-          viewBox="0 0 10 10"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="cart_card-btn-delete-svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            // eslint-disable-next-line max-len
-            d="M9.47205 1.47138C9.7324 1.21103 9.7324 0.788925 9.47205 0.528575C9.21171 0.268226 8.7896 0.268226 8.52925 0.528575L5.00065 4.05717L1.47206 0.528575C1.21171 0.268226 0.789596 0.268226 0.529247 0.528575C0.268897 0.788925 0.268897 1.21103 0.529247 1.47138L4.05784 4.99998L0.529247 8.52857C0.268897 8.78892 0.268897 9.21103 0.529247 9.47138C0.789596 9.73173 1.21171 9.73173 1.47206 9.47138L5.00065 5.94279L8.52925 9.47138C8.7896 9.73173 9.21171 9.73173 9.47205 9.47138C9.7324 9.21103 9.7324 8.78892 9.47205 8.52857L5.94346 4.99998L9.47205 1.47138Z"
-            fill="#B4BDC4"
-          />
-        </svg>
-      </button>
-      <img
-        src={productPhoto}
-        alt="Phone photo"
-        className="cart_card-product-photo"
-      />
-      <p className="cart_card-product-title">
-        Apple iPhone 14 Pro 128GB Silver (MQ023)
-      </p>
-      <div className="cart_options">
-        <button className="cart_card-btn-options">
-          <img
-            src={minus}
-            alt="Minus an item from cart"
-            className="cart_card-btn-options-icon"
-          />
+      <div className="cart_card-info">
+        <button className="cart_card-btn-delete">
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 10 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="cart_card-btn-delete-svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              // eslint-disable-next-line max-len
+              d="M9.47205 1.47138C9.7324 1.21103 9.7324 0.788925 9.47205 0.528575C9.21171 0.268226 8.7896 0.268226 8.52925 0.528575L5.00065 4.05717L1.47206 0.528575C1.21171 0.268226 0.789596 0.268226 0.529247 0.528575C0.268897 0.788925 0.268897 1.21103 0.529247 1.47138L4.05784 4.99998L0.529247 8.52857C0.268897 8.78892 0.268897 9.21103 0.529247 9.47138C0.789596 9.73173 1.21171 9.73173 1.47206 9.47138L5.00065 5.94279L8.52925 9.47138C8.7896 9.73173 9.21171 9.73173 9.47205 9.47138C9.7324 9.21103 9.7324 8.78892 9.47205 8.52857L5.94346 4.99998L9.47205 1.47138Z"
+              fill="#B4BDC4"
+            />
+          </svg>
         </button>
-        <p className="cart_card-options-info">1</p>
-        <button className="cart_card-btn-options">
-          <img src={plus} alt="Plus an item from cart" />
-        </button>
+        <img
+          src={productPhoto}
+          alt="Phone photo"
+          className="cart_card-product-photo"
+        />
+        <p className="cart_card-product-title">
+          Apple iPhone 14 Pro 128GB Silver (MQ023)
+        </p>
       </div>
-      <p className="cart_card-price">$999</p>
+
+      <div className="cart_options-wrapper">
+        <div className="cart_options">
+          <button className="cart_card-btn-options">
+            <img
+              src={minus}
+              alt="Minus an item from cart"
+              className="cart_card-btn-options-icon"
+            />
+          </button>
+          <p className="cart_card-options-info">1</p>
+          <button className="cart_card-btn-options">
+            <img src={plus} alt="Plus an item from cart" />
+          </button>
+        </div>
+        <p className="cart_card-price">$999</p>
+      </div>
     </div>
   );
 };
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import arrowBackIcon from '../../images/arrow-back-icon.svg';
-// eslint-disable-next-line max-len
 import { CartProductCard } from '../../components/CartProductCard/CartProductCard';
 import '../../components/Grid/Grid.scss';
 import './CartPage.scss';
@@ -11,23 +11,31 @@ export const CartPage: React.FC = () => {
     <section className="cart">
       <div className="cart_navigate-wrapper grid grid__item--desktop-1-2">
         <div className="cart_navigate">
-          <Link to={''} className="cart_link">
+          <Link
+            to="#"
+            onClick={() => window.history.back()}
+            className="cart_link"
+          >
             <img src={arrowBackIcon} alt="GoBack" className="cart_back-icon" />
           </Link>
-          <Link to={''} className="cart_link cart_back-title">
+          <Link
+            to="#"
+            onClick={() => window.history.back()}
+            className="cart_link cart_back-title"
+          >
             Back
           </Link>
         </div>
       </div>
       <h1 className="cart_title grid grid__item--desktop-1-3">Cart</h1>
       <div className="cart_info grid">
-        <div className="cart_cards grid__item--desktop-1-16">
+        <div className="cart_cards grid__item--desktop-1-16 grid__item--tablet-1-12 grid__item-1-4">
           <CartProductCard />
           <CartProductCard />
           <CartProductCard />
         </div>
 
-        <div className="card_total-wrapper grid__item--desktop-17-24">
+        <div className="card_total-wrapper grid__item--desktop-17-24 grid__item--tablet-1-12 grid__item-1-4">
           <div className="cart_total">
             <p className="cart_total-price">$2657</p>
             <p className="cart_total-count">Total for 3 items</p>
