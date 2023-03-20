32131f8b8d2b8e999656ed7e226ed77e895856c1
Dashaverz <dashaverzhanskayaa@gmail.com>
2023-03-20 20:48:56 +0100

    added categories

---
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import './CategoryCard.scss';
+import React from 'react';
+import '../Grid/Grid.scss';
+
+type Props = {
+  name: string;
+  itemsCount: number;
+  img: string;
+  path: string;
+};
+
+export const CategoryCard: React.FC<Props> = ({
+  name,
+  itemsCount,
+  img,
+  path,
+}) => {
+  return (
+    // eslint-disable-next-line max-len
+    <div className="category-card grid grid__item--desktop-1-7 grid__item--tablet-1-3 grid__item-1-3">
+      <Link className="category-card_image-link" to={`/${path}`}>
+        <img className="category-card_image" src={img} alt={name} />
+      </Link>
+      <Link className="category-card_title-link" to={`/${path}`}>
+        <h4>{name}</h4>
+      </Link>
+      <p className="category-card_subtitle">{`${itemsCount} models`}</p>
+    </div>
+  );
+};
--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
 import { BannerSlider } from '../../components/BannerSlider';
+import { CategoryCard } from '../../components/CategoryCard';
+import './HomePage.scss';
 
 export const HomePage: React.FC = () => {
+  const categories = [
+    {
+      id: 1,
+      name: 'Mobile phones',
+      itemsCount: 71,
+      img: 'https://i.ibb.co/FbqbFMy/image-phones.png',
+      path: 'phones',
+    },
+    {
+      id: 2,
+      name: 'Tablets',
+      itemsCount: 0,
+      img: 'https://i.ibb.co/1K8StR5/image-tablets.png',
+      path: 'tablets',
+    },
+    {
+      id: 3,
+      name: 'Accessories',
+      itemsCount: 0,
+      img: 'https://i.ibb.co/5WzMHWg/image-accessories.png',
+      path: 'accessories',
+    },
+  ];
+
   return (
     <div className="main-page">
       <h1 className="menu-title">Welcome to Nice Gadgets store!</h1>
       <BannerSlider />
+      <section className="home-page_categories">
+        <div>
+          <h2>Shop by category</h2>
+        </div>
+        <div className="categories-container">
+          {categories.map((category) => (
+            <CategoryCard
+              name={category.name}
+              itemsCount={category.itemsCount}
+              img={category.img}
+              path={category.path}
+              key={category.id}
+            />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
